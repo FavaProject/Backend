d72490f69916a1174945ebd816d337956b4c2e36
roman.nichi.o@gmail.com <F6KrpvTyTjGDqAg>
2024-06-09 01:28:03 +0400

    fava-3: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
 
-    await app.listen(3000);
+  const config = new DocumentBuilder().setTitle('Fava API').build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
