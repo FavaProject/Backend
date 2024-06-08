99621a87321c93f206b9bf333c43ce887dd918af
roman.nichi.o@gmail.com <F6KrpvTyTjGDqAg>
2024-06-09 01:33:24 +0400

    delete unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,4 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module';
-import { User } from './entities/user.entity';
+import { Module } from '@nestjs/common'
 
-@Module({
-    imports: [
-        TypeOrmModule.forRoot({
-            type: 'postgres',
-            host: 'aws-0-eu-central-1.pooler.supabase.com',
-            port: 5432,
-            username: 'postgres.yhckhhhgmkwxzprkaole',
-            password: 'bprsKsSvNTBDVSAl',
-            database: 'postgres',
-            entities: [User],
-            synchronize: true,
-        }),
-        UsersModule,
-    ],
-    controllers: [AppController],
-    providers: [AppService],
-})
+@Module({})
 export class AppModule {}
